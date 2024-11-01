127a7f5db94cc0bd29d72130c10338264a440f8a
GurjeetSingh7000 <101127973+GurjeetSingh7000@users.noreply.github.com>
2024-11-01 22:14:16 +0530

    app index file changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
+import './App.module.scss';
+import BaseLayout from "./components/BaseLayout";
 import {BrowserRouter} from "react-router-dom";
-import MainLayout from './components/MainLayout';
 
 function App() {
    return (
       <div>
          <BrowserRouter>
-             <MainLayout />
-             <div>hi</div>
+            <BaseLayout/>
          </BrowserRouter>
       </div>
    );
